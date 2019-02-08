8ef0e17120de627899c475121332517a5fb87e93
Nash Brancic <nashfft@gmail.com>
2019-02-08 02:29:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
-# your code goes here
+def begins_with_r(array)
+  array = ["ruby", "rspec", "rails", "sails"]
+  array.all? { |word| word[0] == "r" }
+  word[0] = "r"
+end
+
+def contain_a(array)
+  array = ["earth", "fire", "wind", "water", "heart"]
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array = ["candy", :pepper, "wall", :ball, "wacky"]
+  array.find do |word|
+    word[0..1] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array = ["blake", 1, :hello]
+  array.delete_if {|word| !(word.is_a? String)}
+end
+
+def count_elements(array)
+  array = [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
+ array.group_by(&:itself).map{|k, v| k.merge(count: v.length)}
+ end
+ 
+ def merge_data(array)
+   
+ end
+ 
+ def find_cool(array)
+   xena =[]
+   array.each do |element|
+     cool << element if element[:temperature] == "cool"
+   end
+   element
+ end
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
